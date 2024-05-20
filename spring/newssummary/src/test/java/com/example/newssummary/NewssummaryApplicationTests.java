4fa6f2e3ee9609a434ab8a59451c81aaeaac1531
kelgel <alice7644@naver.com>
2024-05-20 21:40:35 +0900

    Initial commit

---
+++ spring/newssummary/src/test/java/com/example/newssummary/NewssummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newssummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewssummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
